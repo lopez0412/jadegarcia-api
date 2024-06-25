f1d4eefc0772caab90788f512f26703efb5ef5e3
Alejandro Lopez <lopez.javier0412@gmail.com>
2024-06-24 21:21:15 -0600

    commit antes de push

--- mensajeria.js
+++ mensajeria.js
@@ -4,12 +4,14 @@ const axios = require('axios');
 const accessToken = 'EAAG05yRobDQBO7GmJ6SYAyMvw6Dr6Na9bJ6FfGBsc6iOcAo8n7nZCQoM6bSG2pvJ18Q20ZABdwWbIyU7r6qpyqasQ9O5J1zOszYEn9gfGGHqWXN2dp1wyOE7jgmIDsS4uuVeY5qUqC2TbZCJhAeKSR9jdaZCa4nuZA4yl0b26jNqR6tds1Jf9QYG6oXnMq70D0ZCqiIZAPHNGojopAAr3YZD';
 const phoneNumberId = '293856593820867';
 
+const userToken = 'EAAG05yRobDQBOycmBp4ZB9D0E9ZCt6ZBdKUNzqElowcVE4a7e24l0jrGGvuecOIQnSGbtYDx9s1PcoX4B7ktEE2iP6M1RE4Bjvm47cAZCoW9yK4QfwZBM34d72wKIpQqUQQnvaNOSxZAfBatPQddYHin2DEJsyPO04gEFtaMsdQD9w8Uc1CxZAPnKXFJnWFaNYPAQZDZD'
+
 
 const sendMessage = async (recipientPhoneNumber, message) => {
     try {
       const url = `https://graph.facebook.com/v13.0/${phoneNumberId}/messages`;
       const headers = {
-        'Authorization': `Bearer ${accessToken}`,
+        'Authorization': `Bearer ${userToken}`,
         'Content-Type': 'application/json',
       };
       const data = {
